2384f29d4507b6d5e54d2b05c9be0e12277b8193
OriStakhiv <orysiastakh@gmail.com>
2018-03-30 07:09:07 +0300

    algoritm and tests ready to use

--- rps.rb
+++ rps.rb
@@ -1,4 +1,40 @@
 require 'rspec'
+class RPS
+  attr_writer :rand_seed
+  def initialize(guess:)
+    @guess = guess.downcase
+  end
+
+  def randomization_algorithm
+    rand 2342343
+  end
+
+  def computer_guess
+    srand(@rand_seed || randomization_algorithm)
+    computer_guesses = %w{rock paper scissors}
+    computer_guesses.sample
+
+  end
+  def winner_is
+    if rule_engine[computer_guess.to_sym].include? @guess
+    'Computer wins'
+    elsif rule_engine[@guess.to_sym].include? computer_guess
+    'You win!'
+    else
+    'Tie'
+    end
+
+  end
+  def rule_engine
+    {
+      'rock': ['scissors'],
+      'paper': ['rock'],
+      'scissors': ['paper']
+
+    }
+  end
+end
+
 
 describe RPS do
   it 'paper beats rock' do
